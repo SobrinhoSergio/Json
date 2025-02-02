037a8bacb670c23d1a6ef85f6fd4bcaac9061ca9
Sérgio de Souza Lima Sobrinho <sobrinhosergio00@gmail.com>
2025-02-01 21:35:15 -0300

    Atualizando proj

---
+++ revisao/cadastro.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', (ev) => {
+    document.getElementById("btnCadastrar")?.addEventListener('click', async (ev) => {
+        var form = new FormData(document.getElementById("formCadastro"));
+        if (validaCampos(form)) {
+            //console.log("Antes await");
+            cadastrarProduto(form);
+            //console.log(produto);
+            //console.log("Depois do depois await");
+        }
+        ev.preventDefault();
+    });
+});
+
+function validaCampos(form) {
+    let campos = ["nome", "preco", "fabricante", "tipoUnidade"];
+    let valido = true;
+    let informacoes = "";
+
+    campos.forEach((campo) => {
+        if (!form.has(campo) || form.get(campo)?.toString().trim() == "") {
+            valido = false;
+            informacoes += `Campo ${campo} não foi preenchido\n`;
+        }
+    });
+    if (informacoes != "") {
+        alert(informacoes);
+    }
+    return valido;
+}
+
+async function cadastrarProduto(produto) {
+    try {
+        const response = await fetch('http://localhost:3000/produtos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(Object.fromEntries(produto)),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erro ao cadastrar produto: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        //setTimeout(()=>{}, 2000);
+        console.log("Dentro da funcao async");
+        alert('Produto cadastrado com sucesso');
+    } catch (error) {
+        console.error('Erro ao cadastrar produto:', error);
+    }
+}
